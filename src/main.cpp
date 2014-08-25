1265a5c1f30a2555b729622ea965d7f1216e5090
Nikolas Engelhard <engelhard@magazino.eu>
2014-08-25 10:19:33 +0200

    added check if camera_frame is in tf-tree

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,28 @@
 
 #include <ros/ros.h>
 #include <iostream>
+#include <tf/transform_listener.h>
+
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
+
+
     ros::init(argc, argv, "pylon_node");
     ros::NodeHandle n;
     std::string  camera_identifier, camera_frame;
         
-        
+    std::string check_frame;
+	  
     n.param<std::string>("camera_identifier", camera_identifier, "*");
     n.param<std::string>("camera_frame", camera_frame, "reference_camera_base");
-               
+    n.param<std::string>("check_frame", check_frame, "insertion_y_visual");
+    
+          
+    tf::TransformListener listener(n,ros::Duration(2.0));
+	
     PylonCameraInterface pylon_cam;
 
     if (!pylon_cam.openCamera(camera_identifier, camera_frame)){
@@ -23,13 +32,26 @@ int main(int argc, char **argv) {
     }
 
     int max_frame_rate = 30;
-
     ros::Rate r(max_frame_rate);
-    while(ros::ok()){
+
+   while(ros::ok()){
         pylon_cam.sendNextImage();
-        r.sleep();
         ros::spinOnce();
+
+
+    ROS_INFO("NEW VERSIONSS");
+
+    /// print warning if given camera frame is not in the tf-tree
+    if ( ! listener.canTransform(camera_frame,check_frame,ros::Time::now()-ros::Duration(0.5)) ) {
+        // ROS_INFO("No trafo between %s and %s, is static_publisher running?",camera_frame.c_str(),check_frame.c_str());
     }
+
+    /// does not work since listener does not forget frames...
+    //         if ((listener.allFramesAsString().find(camera_frame) == std::string::npos)){
+
+     r.sleep();
+      
+   }
 
     // pylon interface is closed in its own deconstructor
     return 0;
