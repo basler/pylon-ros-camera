4abf343970db3ed8b763a2675efa7f363c51ec24
Ulrich Klank <klank@magazino.eu>
2015-07-02 19:43:48 +0200

    fixed usage of wrong service

--- scripts/compatibility_exposure_action.py
+++ scripts/compatibility_exposure_action.py
@@ -12,21 +12,26 @@ class CompAction():
     def __init__(self, camera_name):
        self.camera_name = camera_name
        self._action_name = camera_name+'/calib_exposure_action'
+       rospy.loginfo("Action: " + str(self._action_name))
        self._as = actionlib.SimpleActionServer(self._action_name, maru_msgs.msg.calib_exposureAction, execute_cb=self.execute_cb, auto_start = False)
-       self.exp_srv = rospy.ServiceProxy(camera_name + '/set_exposure_srv', pylon_camera_msgs.srv.SetExposureSrv)
-       if not rospy.wait_for_service(self.exp_srv, timeout=100):
-           rospy.logerr( amera_name + '/set_exposure_srv' + " not available")
+       rospy.loginfo("Service: " + str(camera_name + '/set_brightness_srv'))
+       self.exp_srv = rospy.ServiceProxy(camera_name + '/set_brightness_srv', pylon_camera_msgs.srv.SetBrightnessSrv)
+       
+       rospy.wait_for_service(camera_name + '/set_brightness_srv', timeout=100)
+       #rospy.logerr( camera_name + '/set_exposure_srv' + " not available")
        self._result =  maru_msgs.msg.calib_exposureResult()
        self._feedback =  maru_msgs.msg.calib_exposureFeedback()
        self._as.start()
        
+       
     def execute_cb(self, msg):
-       rospy.loginfo("got exposure request for: " + str(msg.goal_exposure))
+       rospy.loginfo("got brightness request for: " + str(msg.goal_exposure))
        self._as.publish_feedback(self._feedback)       
        try:
-          self.exp_srv(msg.goal_exposure)
+          res = self.exp_srv(msg.goal_exposure)
+          rospy.loginfo("Brightness service result: " +  str(res))
        except Exception, ex:
-          print ex
+          rospy.logerr(str(ex))
        self._as.set_succeeded(self._result)
        
     
