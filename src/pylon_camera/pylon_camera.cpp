d26514da8e0dc61803e5698d8a11147a82190687
Nikolas Engelhard <engelhard@magazino.eu>
2015-11-17 16:50:34 +0100

    camera now also opens if no camera_name was written into it

--- src/pylon_camera/pylon_camera.cpp
+++ src/pylon_camera/pylon_camera.cpp
@@ -126,8 +126,11 @@ PylonCamera* PylonCamera::create(const std::string& name)
 
                 camera_array[i].Close();
 
-                if (device_user_id.compare(name) == 0 || 
-                    device_user_id.compare(device_user_id.length() - name.length(), name.length(), name))
+//                std::cout << "device_user_id " << device_user_id << std::endl;
+//                std::cout << "length " << device_user_id.size() << std::endl;
+
+                if (device_user_id.compare(name) == 0) // ||
+//                    device_user_id.compare(device_user_id.length() - name.length(), name.length(), name))
                 {
                     found_desired_device = true;
                     cam_pos = i;
