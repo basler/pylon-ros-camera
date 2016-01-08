4e5b85b02a7d9c5dff4580e94edf443d4695c440
Markus Grimm <grimm@magazino.eu>
2016-01-08 13:05:10 +0100

    Deleted maru_frame_rate_tester.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     # #  don't do this unless you want a globally visible script
-    scripts=['scripts/maru_frame_rate_tester.py'], 
     #packages=[],
     #package_dir={}
   )
