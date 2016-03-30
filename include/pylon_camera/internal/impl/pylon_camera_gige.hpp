2be065ad6ea0b521c45cca97ca86d1a30e6f9b25
Marcel Debout <debout@magazino.eu>
2016-03-30 15:41:26 +0200

    added missing 'All rights reserved' tag, added LICENSE.rst file

--- include/pylon_camera/internal/impl/pylon_camera_gige.hpp
+++ include/pylon_camera/internal/impl/pylon_camera_gige.hpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Software License Agreement (BSD License)
  *
- * Copyright (C) 2016, Magazino GmbH
+ * Copyright (C) 2016, Magazino GmbH. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
