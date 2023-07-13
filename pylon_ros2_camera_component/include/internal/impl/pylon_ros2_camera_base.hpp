bec1bffc3d2e7080fe0cab99a97c5eb283f1999b
Alireza Moayyedi <alireza.moayyedi@nobleo.nl>
2023-07-13 14:06:34 +0200

    stop grabbing to set user set

--- pylon_ros2_camera_component/include/internal/impl/pylon_ros2_camera_base.hpp
+++ pylon_ros2_camera_component/include/internal/impl/pylon_ros2_camera_base.hpp
@@ -2614,6 +2614,7 @@ std::string PylonROS2CameraImpl<CameraTraitT>::setUserSetSelector(const int& set
 {
     try
     {
+        grabbingStopping();
         if ( GenApi::IsAvailable(cam_->UserSetSelector))
         {  
             if (set == 0)
@@ -2654,6 +2655,7 @@ std::string PylonROS2CameraImpl<CameraTraitT>::setUserSetSelector(const int& set
              RCLCPP_ERROR_STREAM(LOGGER_BASE, "Error while trying to select the user set. The connected Camera not supporting this feature");
              return "The connected Camera not supporting this feature";
         }
+        grabbingStarting();
     }
     catch ( const GenICam::GenericException &e )
     {
