5cf7ee5a9735f848552dff990ad33a72a5ab3cf7
Gaël Écorchard <gael@km-robotics.cz>
2024-04-26 08:04:30 +0200

    Add the respawn argument to the launch file
    It was hard-coded to False before and this is still the default.
    
    Signed-off-by: Gaël Écorchard <gael@km-robotics.cz>

--- pylon_ros2_camera_wrapper/launch/pylon_ros2_camera.launch.py
+++ pylon_ros2_camera_wrapper/launch/pylon_ros2_camera.launch.py
@@ -6,32 +6,71 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.actions import OpaqueFunction
+from launch.launch_context import LaunchContext
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
-def generate_launch_description():
+def _launch_node(context: LaunchContext):
+    """Return the action to launch `pylon_ros2_camera_wrapper`.
+
+    This is required to evaluate `respawn` as boolean.
+
+    """
 
     # adapt if needed
     debug = False
-    respawn = False
 
-    default_config_file = os.path.join(
-        get_package_share_directory('pylon_ros2_camera_wrapper'),
-        'config',
-        'default.yaml'
-    )
-
-    # launch configuration variables
     node_name = LaunchConfiguration('node_name')
     camera_id = LaunchConfiguration('camera_id')
-
     config_file = LaunchConfiguration('config_file')
 
     mtu_size = LaunchConfiguration('mtu_size')
     startup_user_set = LaunchConfiguration('startup_user_set')
     enable_status_publisher = LaunchConfiguration('enable_status_publisher')
     enable_current_params_publisher = LaunchConfiguration('enable_current_params_publisher')
+
+    respawn = LaunchConfiguration('respawn')
+    respawn_str = respawn.perform(context)
+    respawn_bool = respawn_str.lower() == 'true'
+
+    # see https://navigation.ros.org/tutorials/docs/get_backtrace.html
+    if debug:
+        launch_prefix = ['xterm -e gdb -ex run --args']
+    else:
+        launch_prefix = ''
+
+    return [
+            Node(
+                package='pylon_ros2_camera_wrapper',
+                namespace=camera_id,
+                executable='pylon_ros2_camera_wrapper',
+                name=node_name,
+                output='screen',
+                respawn=respawn_bool,
+                emulate_tty=True,
+                prefix=launch_prefix,
+                parameters=[
+                    config_file,
+                    {
+                        'gige/mtu_size': mtu_size,
+                        'startup_user_set': startup_user_set,
+                        'enable_status_publisher': enable_status_publisher,
+                        'enable_current_params_publisher': enable_current_params_publisher
+                    }
+                ]
+            ),
+        ]
+
+
+def generate_launch_description():
+
+    default_config_file = os.path.join(
+        get_package_share_directory('pylon_ros2_camera_wrapper'),
+        'config',
+        'default.yaml'
+    )
 
     # launch arguments
     declare_node_name_cmd = DeclareLaunchArgument(
@@ -77,34 +116,10 @@ def generate_launch_description():
         description='Enable/Disable the current parameter publishing.'
     )
 
-    # log format
-    os.environ['RCUTILS_CONSOLE_OUTPUT_FORMAT'] = '{time} [{name}] [{severity}] {message}'
-
-    # see https://navigation.ros.org/tutorials/docs/get_backtrace.html
-    if (debug == True):
-        launch_prefix = ['xterm -e gdb -ex run --args']
-    else:
-        launch_prefix = ''
-
-    # node
-    pylon_ros2_camera_node = Node(
-        package='pylon_ros2_camera_wrapper',        
-        namespace=camera_id,
-        executable='pylon_ros2_camera_wrapper',
-        name=node_name,
-        output='screen',
-        respawn=respawn,
-        emulate_tty=True,
-        prefix=launch_prefix,
-        parameters=[
-            config_file,
-            {
-                'gige/mtu_size': mtu_size,
-                'startup_user_set': startup_user_set,
-                'enable_status_publisher': enable_status_publisher,
-                'enable_current_params_publisher': enable_current_params_publisher
-            }
-        ]
+    declare_respawn_cmd = DeclareLaunchArgument(
+        'respawn',
+        default_value='false',
+        description='If true, the node will be respawned if it exits.'
     )
 
     # Define LaunchDescription variable and return it
@@ -119,6 +134,8 @@ def generate_launch_description():
     ld.add_action(declare_enable_status_publisher_cmd)
     ld.add_action(declare_enable_current_params_publisher_cmd)
 
-    ld.add_action(pylon_ros2_camera_node)
+    ld.add_action(declare_respawn_cmd)
+
+    ld.add_action(OpaqueFunction(function=_launch_node))
 
     return ld
